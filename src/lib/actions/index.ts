7b54c0a4c7ae71fcacd6308cd738fd1f8437a2c5
hoangtrung1002 <n.jordan1002@gmail.com>
2024-05-15 23:47:34 +0700

    feat: upload image, remove and update profile

--- src/lib/actions/index.ts
+++ src/lib/actions/index.ts
@@ -1,11 +1,51 @@
 "use server";
 
+import { currentUser } from "@clerk/nextjs";
 import { db } from "../db";
 
-const removeProfileImage = async () => {
+export const removeProfileImage = async () => {
+  const authUser = await currentUser();
+  if (!authUser) return;
+
   const response = await db.user.update({
     where: { clerkId: authUser.id },
     data: { profileImage: "" },
   });
   return response;
+};
+
+export const uploadProfileImage = async (image: string) => {
+  const authUser = await currentUser();
+
+  if (!authUser) return;
+
+  try {
+    const id = authUser.id;
+    const response = await db.user.update({
+      where: {
+        clerkId: id,
+      },
+      data: {
+        profileImage: image,
+      },
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateUserInfo = async (name: string) => {
+  const authUser = await currentUser();
+  if (!authUser) return;
+  try {
+    const updateUser = await db.user.update({
+      where: { clerkId: authUser.id },
+      data: { name },
+    });
+    return updateUser;
+  } catch (error) {
+    console.error(error);
+  }
 };
